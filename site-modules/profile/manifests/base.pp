3204b95967645cf10196a171bcd6e9af3bbb5c78
Robert Waffen <rwaffen@gmail.com>
2021-12-03 12:35:52 +0100

    remove default etc host entries

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -5,4 +5,18 @@ class profile::base {
   package { 'glibc-langpack-de':
     ensure => installed
   }
+
+  host { 'remove default 127.0.0.1 fqdn entries':
+    ensure       => absent,
+    name         => $facts['networking']['fqdn'],
+    host_aliases => $facts['networking']['hostname'],
+    ip           => '127.0.0.1',
+  }
+
+  host { 'remove default ::1 fqdn entries':
+    ensure       => absent,
+    name         => $facts['networking']['fqdn'],
+    host_aliases => $facts['networking']['hostname'],
+    ip           => '::1',
+  }
 }
