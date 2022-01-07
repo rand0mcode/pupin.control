33dd594102c976a153e89098710fff0d2f01be9c
Robert Waffen <rwaffen@gmail.com>
2022-01-07 15:08:14 +0100

    update r10k config

--- site-modules/profile/manifests/puppet/compiler.pp
+++ site-modules/profile/manifests/puppet/compiler.pp
@@ -10,12 +10,10 @@ class profile::puppet::compiler (
   class { 'r10k':
     sources => {
       puppet => {
-        remote  => $control_repo,
-        basedir => '/etc/puppetlabs/code/environments',
-        prefix  => false,
-        deploy  => {
-          generate_types => true,
-        },
+        remote         => $control_repo,
+        basedir        => '/etc/puppetlabs/code/environments',
+        prefix         => false,
+        generate_types => true,
       },
     },
   }
