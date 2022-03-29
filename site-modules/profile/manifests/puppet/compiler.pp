8ba994380911de2d8584310989d16e55cad502d1
Robert Waffen <rwaffen@gmail.com>
2022-03-29 14:57:28 +0200

    update compiler profile

--- site-modules/profile/manifests/puppet/compiler.pp
+++ site-modules/profile/manifests/puppet/compiler.pp
@@ -4,20 +4,24 @@
 class profile::puppet::compiler (
   String $puppetdb_host,
   String $control_repo,
+  String $r10k_version,
+  Array[String[1]] $r10k_purge,
 ){
   include git
 
   class { 'r10k':
-    sources => {
-      puppet => {
-        remote  => $control_repo,
-        basedir => '/etc/puppetlabs/code/environments',
-        prefix  => false,
-        deploy  => {
-          generate_types => true,
-        },
-      },
-    },
+    remote          => $control_repo,
+    version         => $r10k_version,
+    deploy_settings => {
+      purge_levels   => $r10k_purge,
+      generate_types => true,
+    }
+  }
+
+  cron { 'update_puppet_envs':
+    ensure  => present,
+    command => '/opt/puppetlabs/puppet/bin/r10k deploy environment -pv > /var/log/puppetlabs/last_r10k_cron_run.log 2>&1',
+    minute  => '*/10',
   }
 
   class { 'puppetdb::master::config':
