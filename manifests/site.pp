087e7d5b78a5f1642ed5a4315f3572216af7192e
Robert Waffen <rwaffen@gmail.com>
2022-03-30 13:30:36 +0200

    try try-catch

--- manifests/site.pp
+++ manifests/site.pp
@@ -27,7 +27,7 @@ File { backup => false }
 node default {
   $ret = try() || {
     include "role::${trusted['extensions']['pp_role']}"
-  }.catch('EvaluationError', 'ServerError') |$exception| {
+  }.catch |$exception| {
     notify { 'role not found, using default role':}
     include 'role::default'
   }
