51284de35bf0cd45f78c0d18c8c3f68d7ff6b96e
Robert Waffen <rwaffen@gmail.com>
2022-07-26 13:11:57 +0200

    test source of package

--- site-modules/profile/manifests/database/oracle.pp
+++ site-modules/profile/manifests/database/oracle.pp
@@ -10,10 +10,11 @@
 #
 class profile::database::oracle (
   Stdlib::HTTPSUrl $preinstall_pkg = 'https://yum.oracle.com/repo/OracleLinux/OL8/appstream/x86_64/getPackage/oracle-database-preinstall-19c-1.0-2.el8.x86_64.rpm',
-  String[1] $provider = 'rpm',
+  # String[1] $provider = 'rpm',
 ) {
-  package { $preinstall_pkg:
+  package { 'oracle-database-preinstall-19c':
     ensure   => 'installed',
-    provider => $provider,
+    source   => $preinstall_pkg,
+    # provider => $provider,
   }
 }
