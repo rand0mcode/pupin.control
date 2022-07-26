10dd81d4d18b76cac5b83d0d0a858549751c538d
Robert Waffen <rwaffen@gmail.com>
2022-07-26 13:06:24 +0200

    change to provider rpm, because dnf does not like to install from internet

--- site-modules/profile/manifests/database/oracle.pp
+++ site-modules/profile/manifests/database/oracle.pp
@@ -4,10 +4,16 @@
 # @param preinstall_pkg
 #   url of the rpm for preinstallation dependencies
 #
+# @param provider
+#   provider to use for pakacge installation
+#   defaults to rpm, because yum/dnf does not seem to work
+#
 class profile::database::oracle (
   Stdlib::HTTPSUrl $preinstall_pkg = 'https://yum.oracle.com/repo/OracleLinux/OL8/appstream/x86_64/getPackage/oracle-database-preinstall-19c-1.0-2.el8.x86_64.rpm',
+  String[1] $provider = 'rpm',
 ) {
   package { $preinstall_pkg:
-    ensure => 'installed',
+    ensure   => 'installed',
+    provider => $provider,
   }
 }
